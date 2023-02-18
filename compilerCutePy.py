94dec9fb11bb87a2f60348985d948e8f8cfb58c0
Thanos <thmasouras@gmail.com>
2023-02-18 14:48:40 +0200

    started states logic

--- compilerCutePy.py
+++ compilerCutePy.py
@@ -1,5 +1,6 @@
 import sys
-
+## GLOBAL DECLERATIONS ##
+temporary_states = {0,1,2,3,4,5,6}
 
 class Token:
     def __init__(self, category, value, line):
@@ -21,7 +22,39 @@ def open_cpi_file():
 	file = open(sys.argv[1], "r")
 
 def lex():
-	print("Lex hi")
+
+	
+	word = []
+	state = 0
+
+	while state in temporary_states:
+		
+		char = file.read(1)
+		word.append(char)
+
+		if 1==1:
+			if char.isspace():
+				state = 0
+			if char.isalpha():
+				state = 1
+			if char.isdigit():
+				state = 2
+			if char == '<':
+				state = 3
+			if char == '>':
+				state = 4
+			if char == ':':
+				state = 5
+			if char == '#':
+				state = 6
+			if char in ('+', '-', '*', '/', '=', ',', ';', '{', '}', '(', ')', '[', ']'):
+				state = 0 #temp value
+			
+		if char == '':
+			sys.exit()
+		print ("Char is: " + char + " and State is: ", state)
+
+
 
 
 def close_files():
