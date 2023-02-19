3c2801163ca841511606f31ff8b482000be34efa
Thanos <thmasouras@gmail.com>
2023-02-19 20:30:21 +0200

    added token types

--- compilerCutePy.py
+++ compilerCutePy.py
@@ -16,33 +16,76 @@ reserved_words_list = [
     'input',
     'print']
 
+tokens_dict = {
+    '+' : 'TOKEN_plus',
+    '-' : 'TOKEN_minus',
+    '*' : 'TOKEN_times',
+    '/' : 'TOKEN_slash',
+    '<' : 'TOKEN_less',
+    '>' : 'TOKEN_greater',
+    '=' : 'TOKEN_equal',
+    '<>': 'TOKEN_nonEqual',
+    '<=' : 'TOKEN_lessEqual',
+    '>=' : 'TOKEN_greaterEqual',
+    '(' : 'TOKEN_leftP',
+    ')' : 'TOKEN_rightP',
+    '[' : 'TOKEN_leftBracket',
+    ']' : 'TOKEN_rightBracket',
+    '{' : 'TOKEN_leftBrace',
+    '}' : 'TOKEN_rightBrace',
+    ';' : 'TOKEN_semiColon',
+    ':' : 'TOKEN_colon',
+    ',' : 'TOKEN_comma',
+    '.' : 'TOKEN_programm_end',
+    'declare' : 'TOKEN_declare',
+    'def' : 'TOKEN_def',
+    'if' : 'TOKEN_if',
+    'else' : 'TOKEN_else',
+    'while' : 'TOKEN_while',
+    'default' : 'TOKEN_default',
+    'not' : 'TOKEN_not',
+    'and' : 'TOKEN_and',
+    'or' : 'TOKEN_or',
+    'return' : 'TOKEN_return',
+    'input' : 'TOKEN_input',
+    'print' : 'TOKEN_print',
+    'EOF' : 'TOKEN_eof',
+    'ID' : 'TOKEN_id',
+    "number" : 'TOKEN_number',
+    '.' : 'TOKEN_dot',
+    '#$' : 'TOKEN_comment',
+    '#{' : 'TOKEN_left_hashbracket',
+    '#}' : 'TOKEN_rigth_hashbracket',
+    '#' : 'TOKEN_hashtag'
+}
+
 class Token:
     def __init__(self, category, value, line):
         self.type = category
         self.value = value
         self.line = line
 
-    def get_token(self):
+    def get_token_type(self):
         return self.token_type
 
 def open_cpy_file():
 	global file
+
 	if len(sys.argv) < 2:
 		print("Error: There is no input file")
 		sys.exit()
 	if sys.argv[1][-3:] != "cpy":
 		print("Error: Input file is not a CutePy file")
 		sys.exit()
+		
 	file = open(sys.argv[1], "r")
 
 def lex():
-
 	word = []
 	state = 0
 	global line
 	move_file_pointer = False
 	while state in temporary_states:
-		
 		previus_pos = file.tell()
 		char = file.read(1)
 		word.append(char)
@@ -93,14 +136,22 @@ def lex():
 		del word[-1]
 		file.seek(previus_pos)
 	if state == final:
-			print(word)
+			word = ''.join(word)
+			create_token(word,line)
 	if char != "":
 		lex()
 
 
 
+def create_token(word,line):
 
-
+	if word in tokens_dict.keys():
+		token = Token(tokens_dict[word], word, line)
+	elif word.isdigit():
+		token = Token(tokens_dict['number'], word, line)
+	else:
+		token = Token(tokens_dict['ID'], word, line)
+	print(' LINE: %d :: '  %token.line + token.type + ' :: ' + token.value)
 
 def close_files():
 	file.close()
--- testExamples.cpy
+++ testExamples.cpy
@@ -1,4 +1,17 @@
-x=int (input());
-	fact=1;
-	i=1;
-	while (i<=x):
+def main_factorial():
+#{
+    #$ declarations #$
+    #declare x
+    #declare i,fact
+
+    #$body of main_factorial#$
+    x = int(input());
+    fact = 1;
+    i=1;
+    while(i>=x):
+    #{
+        fact=fact*i;
+        i=i+1;
+    #}
+    print(fact);
+#}
